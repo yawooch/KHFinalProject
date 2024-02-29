fa44d3566495b031b49f2cf4b31856da9925c34a
DESKTOP-DBDPV3E\user1 <yoc0421@naver.com>
2024-02-29 17:49:51 +0900

    Stay/ Trip DB 컬럼 추가 VO 수정

--- springProject/PawPawTrip/src/main/java/com/kr/pawpawtrip/trip/model/vo/Spot.java
+++ springProject/PawPawTrip/src/main/java/com/kr/pawpawtrip/trip/model/vo/Spot.java
@@ -9,53 +9,63 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Spot {	// DB 테이블명 = TRIP
-	
-	// 여행 콘텐츠 ID (기본키)
-	private int tripContentId;
-	
-	// 여행지 주소
-	private String tripAddress;
-	
-	// 여행지 이름
-	private String tripTitle;
-	
-	// 지역 코드
-	private int areaCode;
-	
-	// 여행 카테고리 대분류
-	private String tripCategory1;
-		
-	// 여행 카테고리 중분류	
-	private String tripCategory2;
-	
-	// 여행 카테고리 소분류	
-	private String tripCategory3;
-	
-	// 여행 콘텐츠 타입 ID
-	private int tripContentTypeId;
-	
-	// 등록일
-	private LocalDate tripCreateTime;
-	
-	// 수정일
-	private LocalDate tripModifyTime;
-	
-	// 여행 이미지
-	private String tripImage;
-	
-	// x좌표
-	private int mapX;
-	
-	// y좌표
-	private int mapY;
-	
-	// 축척
-	private int mapLevel;
-	
-	// 여행 전화번호
-	private String tripTel;
-	
-	// 시군구 코드
-	private int sigunguCode;
+public class Spot
+{ // DB 테이블명 = TRIP
+
+    // 여행 콘텐츠 ID (기본키)
+    private int tripContentId;
+
+    // 여행지 주소
+    private String tripAddress;
+
+    // 여행지 상세주소
+    private String tripDetailAddress;
+
+    // 여행지 이름
+    private String tripTitle;
+
+    // 지역 코드
+    private int areaCode;
+
+    // 여행 카테고리 대분류
+    private String tripCategory1;
+
+    // 여행 카테고리 중분류
+    private String tripCategory2;
+
+    // 여행 카테고리 소분류
+    private String tripCategory3;
+
+    // 여행 콘텐츠 타입 ID
+    private int tripContentTypeId;
+
+    // 등록일
+    private LocalDate tripCreateTime;
+
+    // 수정일
+    private LocalDate tripModifyTime;
+
+    // 여행 이미지
+    private String tripImage;
+
+    // x좌표
+    private int mapX;
+
+    // y좌표
+    private int mapY;
+
+    // 축척
+    private int mapLevel;
+
+    // 여행 전화번호
+    private String tripTel;
+
+    // 시군구 코드
+    private int sigunguCode;
+    
+    // 홈페이지주소
+    private int homepage;
+    
+    // 소개설명
+    private int overview;
 }
--- springProject/PawPawTrip/src/main/java/com/kr/pawpawtrip/trip/model/vo/Stay.java
+++ springProject/PawPawTrip/src/main/java/com/kr/pawpawtrip/trip/model/vo/Stay.java
@@ -9,56 +9,62 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Stay {
-	
-	// 숙박 콘텐츠 ID (기본키)
-	private int stayContentId;
-	
-	// 숙박 주소
-	private String stayAddress;
-	
-	// 숙박 상세주소 
-	// private String stayDetailAddress;
-	
-	// 숙소 이름
-	private String stayTitle;
-	
-	// 지역 코드
-	private int areaCode;
-	
-	// 숙박 카테고리 대분류
-	private String stayCategory1;
-	
-	// 숙박 카테고리 중분류
-	private String stayCategory2;
-	
-	// 숙박 카테고리 소분류
-	private String stayCategory3;
-	
-	// 숙박 콘텐츠 타입ID
-	private int stayContentTypeId;
-	
-	// 등록일
-	private LocalDate stayCrateTime;
-	
-	// 수정일
-	private LocalDate stayModifyTime;
-	
-	// 숙소 이미지
-	private String stayImage;
-	
-	// x좌표
-	private int mapX;
-	
-	// y좌표
-	private int mapY;
-	
-	// 축척
-	private int mapLevel;
-	
-	// 숙박 전화번호
-	private String stayTel;
-	
-	// 시군구 코드
-	private int sigunguCode;
+public class Stay
+{
+    // 숙박 콘텐츠 ID (기본키)
+    private int stayContentId;
+
+    // 숙박 주소
+    private String stayAddress;
+
+    // 숙박 상세주소
+    private String stayDetailAddress;
+
+    // 숙소 이름
+    private String stayTitle;
+
+    // 지역 코드
+    private int areaCode;
+
+    // 숙박 카테고리 대분류
+    private String stayCategory1;
+
+    // 숙박 카테고리 중분류
+    private String stayCategory2;
+
+    // 숙박 카테고리 소분류
+    private String stayCategory3;
+
+    // 숙박 콘텐츠 타입ID
+    private int stayContentTypeId;
+
+    // 등록일
+    private LocalDate stayCrateTime;
+
+    // 수정일
+    private LocalDate stayModifyTime;
+
+    // 숙소 이미지
+    private String stayImage;
+
+    // x좌표
+    private int mapX;
+
+    // y좌표
+    private int mapY;
+
+    // 축척
+    private int mapLevel;
+
+    // 숙박 전화번호
+    private String stayTel;
+
+    // 시군구 코드
+    private int sigunguCode;
+
+    // 홈페이지주소
+    private int homepage;
+
+    // 소개설명
+    private int overview;
 }
