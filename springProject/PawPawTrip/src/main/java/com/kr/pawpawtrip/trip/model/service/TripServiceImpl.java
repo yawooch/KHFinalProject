b9347e360d4a10323f70138bb2b54a095b7b76b0
DESKTOP-A1PLBHD\jeong <jeongah0611@gmail.com>
2024-03-03 19:59:01 +0900

    김정아 컴잇

--- springProject/PawPawTrip/src/main/java/com/kr/pawpawtrip/trip/model/service/TripServiceImpl.java
+++ springProject/PawPawTrip/src/main/java/com/kr/pawpawtrip/trip/model/service/TripServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.kr.pawpawtrip.common.util.PageInfo;
 import com.kr.pawpawtrip.trip.model.mapper.TripMapper;
 import com.kr.pawpawtrip.trip.model.vo.Comm;
 import com.kr.pawpawtrip.trip.model.vo.PetInfo;
@@ -19,13 +20,22 @@ public class TripServiceImpl implements TripService {
 	
 	private final TripMapper tripMapper;
 	
-	// 여행지 전체 조회
+	// 여행지 전체 게시물 수 조회
 	@Override
-	public List<Spot> getSpotList() {
+	public int getSpotCount(String select, String search) {
 		
-		return tripMapper.selectAll();
+		return 0;
 	}
-
+	
+	
+	// 여행지 전체 조회(선택/검색 기능 포함)
+	@Override
+	public List<Spot> getSpotList(PageInfo pageInfo, String select, String search) {
+		
+		return tripMapper.selectSpotList();
+	}
+	
+	
     @Override
     @Transactional
     public int savePetInfo(PetInfo petInfo)
@@ -42,7 +52,8 @@ public class TripServiceImpl implements TripService {
         
         return result;
     }
-
+    
+    
     @Override
     @Transactional
     public int saveTrip(Spot trip)
@@ -59,7 +70,8 @@ public class TripServiceImpl implements TripService {
         
         return result;
     }
-
+    
+    
     @Override
     @Transactional
     public int saveStay(Stay stay)
@@ -76,7 +88,8 @@ public class TripServiceImpl implements TripService {
         
         return result;
     }
-
+    
+    
     @Override
     public int saveComm(Comm comm)
     {
@@ -92,10 +105,14 @@ public class TripServiceImpl implements TripService {
       
       return result;
     }
-
+    
+    
     @Override
     public List<PetInfo> getPetTourListByContentId(String contentIdQuery)
     {
         return tripMapper.selectPetInfoByContentId(contentIdQuery);
     }
+
+
+
 }
